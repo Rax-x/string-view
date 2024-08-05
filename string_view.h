9c92f60cebecad5f162e0abf5484d8e4f3c4051a
none <none>
2024-08-05 20:50:13 +0200

    refactor: Move to stb style

--- string_view.h
+++ string_view.h
@@ -1,59 +1,202 @@
 #ifndef _STRING_VIEW_H_
-    #define _STRING_VIEW_H_
+#define _STRING_VIEW_H_
 
-    #include <stdint.h>
-    #include <stdbool.h>
-
-    #ifndef NULL
-        #ifndef _cplusplus
-            #define NULL ((void *)0)
-        #else
-            #define NULL 0
-        #endif
-    #endif
-
-    #if defined _WIN32 || defined _WIN64 
-        #define SV_API __declspec(dllexport)
-    #else 
-        #define SV_API extern
-    #endif
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdio.h>
     
+#define STRING_VIEW_FORMAT "%.*s"
+#define STRING_VIEW_ARG(sv) (sv).count, (sv).data
 
-    #define SV_FORMAT "%.*s"
-    #define SV_ARG(sv) (sv).size, (sv).data
+#define STRING_VIEW_NULL ((string_view_t) {.data = NULL, .count = 0})
 
-    #define SV_NULL (string_view) {.data = NULL, .size = 0}
+#define STRING_VIEW_NPOS ((size_t)-1)
 
-    typedef struct _string_view{
-        char* data;
-        uint32_t size;
-    }string_view;
+typedef struct _string_view {
+    const char* data;
+    size_t count;
+} string_view_t;
 
-    SV_API string_view sv_from_parts(char* data, uint32_t size);
-    SV_API string_view sv_from_str(char* data);
+string_view_t new_string_view(const char* data, size_t count);
+string_view_t new_string_view_from_cstr(const char* data);
     
-    SV_API char* sv_data(string_view sv);
-    SV_API uint32_t sv_size(string_view sv);
+const char* string_view_data(string_view_t sv);
+size_t string_view_size(string_view_t sv);
 
-    SV_API char sv_at(string_view sv, uint32_t index);
-    SV_API char sv_front(string_view sv);
-    SV_API char sv_back(string_view sv);
+char string_view_at(string_view_t sv, size_t index);
+char string_view_front(string_view_t sv);
+char string_view_back(string_view_t sv);
     
-    SV_API bool sv_is_empty(string_view sv);
-    SV_API bool sv_is_null(string_view sv);
+bool string_view_is_empty(string_view_t sv);
 
-    SV_API void sv_trim(string_view *sv);
-    SV_API void sv_remove_prefix(string_view *sv, uint32_t pos);
-    SV_API void sv_remove_suffix(string_view *sv, uint32_t pos);  
+void string_view_trim_left(string_view_t *sv);
+void string_view_trim_right(string_view_t *sv);
+void string_view_trim(string_view_t *sv);
+
+void string_view_remove_prefix(string_view_t *sv, size_t pos);
+void string_view_remove_suffix(string_view_t *sv, size_t pos);  
     
-    SV_API void sv_swap(string_view* sv1, string_view* sv2); 
-    SV_API void sv_copy(string_view sv, char *dest, uint32_t size, uint32_t pos);
-    SV_API string_view sv_substr(string_view sv, uint32_t pos, uint32_t count);
-    SV_API int8_t sv_compare(string_view sv1, string_view sv2);
-    SV_API bool sv_equal(string_view sv1, string_view sv2);
-    SV_API bool sv_starts_with(string_view sv, const char* prefix, uint32_t len);
-    SV_API bool sv_ends_with(string_view sv, const char* suffix, uint32_t len);
-    SV_API int32_t sv_find_char(string_view sv, char c, uint32_t pos);
-    SV_API int32_t sv_find_str(string_view sv, char* str, uint32_t pos);
+void string_view_swap(string_view_t* sv1, string_view_t* sv2); 
+void string_view_copy(string_view_t sv, char *dest, size_t size, size_t pos);
+
+string_view_t string_view_substr(string_view_t sv, size_t pos, size_t count);
+
+int string_view_compare(string_view_t sv1, string_view_t sv2);
+bool string_view_equal(string_view_t sv1, string_view_t sv2);
+
+bool string_view_starts_with(string_view_t sv, const char* prefix, size_t len);
+bool string_view_ends_with(string_view_t sv, const char* suffix, size_t len);
+
+size_t string_view_find_char(string_view_t sv, char c, size_t pos);
+size_t string_view_find_str(string_view_t sv, char* str, size_t pos);
+
+#endif
+
+#ifdef STRING_VIEW_IMPLEMENTATION
+
+#include <ctype.h>
+#include <string.h>
+
+string_view_t new_string_view(const char* data, size_t size){
+    return (string_view_t) {
+        .data = data,
+        .count = size
+    };
+}
+
+string_view_t new_string_view_from_cstr(const char* data){
+    return new_string_view(data, strlen(data));
+}
+
+char string_view_at(string_view_t sv, size_t index){
+    return index < sv.count
+        ? sv.data[index]
+        : '\0';
+}
+
+char string_view_front(string_view_t sv){    
+    return sv.data[0];
+}
+
+char string_view_back(string_view_t sv){
+    return sv.data[sv.count - 1];
+}
+
+const char* string_view_data(string_view_t sv){
+    return sv.data;
+}
+
+size_t string_view_size(string_view_t sv){
+    return sv.count;
+}
+
+bool string_view_is_empty(string_view_t sv){
+    return sv.count == 0;
+}
+
+void string_view_trim_left(string_view_t *sv) {
+
+    while(isspace(sv->data[0])){
+        sv->data++;
+        sv->count--;
+    }
+}
+
+void string_view_trim_right(string_view_t *sv) {
+    while(isspace(string_view_back(*sv))){
+        sv->count--;
+    }
+}
+
+void string_view_trim(string_view_t *sv){
+    string_view_trim_left(sv);
+    string_view_trim_right(sv);
+}
+
+void string_view_remove_prefix(string_view_t *sv, size_t pos){
+    if(pos > sv->count){
+        *sv = STRING_VIEW_NULL;
+    }else{
+        sv->data += pos;
+        sv->count -= pos;
+    }
+}
+
+void string_view_remove_suffix(string_view_t *sv, size_t pos){
+    sv->count -= (pos > sv->count)
+        ? sv->count
+        : pos;
+}
+
+void string_view_swap(string_view_t* sv1, string_view_t* sv2){
+    string_view_t tmp = *sv1;
+    *sv1 = *sv2;
+    *sv2 = tmp;
+}
+
+void string_view_copy(string_view_t sv, char *dest, size_t size, size_t pos){
+
+    if(dest == NULL || pos > sv.count) return;
+    if(size > sv.count) size = sv.count - pos;
+
+    memcpy(dest, &sv.data[pos], size);
+}
+
+string_view_t string_view_substr(string_view_t sv, size_t pos, size_t count){  
+    if(count > (sv.count - pos)) {
+        count = sv.count - pos;
+    }
+
+    return (pos < sv.count) 
+        ? new_string_view(&sv.data[pos], count)
+        : STRING_VIEW_NULL;
+}
+
+int string_view_compare(const string_view_t sv1, const string_view_t sv2){
+    
+    if(sv1.count > sv2.count) return 1;
+    if(sv1.count < sv2.count) return -1;
+    
+    return strncmp(sv1.data, sv2.data, sv1.count);
+}
+
+bool string_view_equal(string_view_t sv1, string_view_t sv2){
+    return string_view_compare(sv1, sv2) == 0;
+}
+
+bool string_view_starts_with(string_view_t sv, const char* prefix, size_t len){
+    if(len > sv.count) return false;
+    return strncmp(sv.data, prefix, len) == 0;
+}
+
+
+bool string_view_ends_with(string_view_t sv, const char* suffix, size_t len){
+    
+    if(len > sv.count) return false;
+
+    const size_t pos = sv.count - len;
+    return strncmp(&sv.data[pos], suffix, len) == 0;
+}
+
+size_t string_view_find_char(string_view_t sv, char c, size_t pos){
+
+    for(size_t i = pos; i < sv.count; i++){
+        if(sv.data[i] == c) return i;
+    }
+
+    return STRING_VIEW_NPOS;
+}
+
+size_t string_view_find_str(string_view_t sv, char* str, size_t pos){
+    const size_t length = strlen(str);
+
+    for(size_t i = pos; i < sv.count ; i++){
+        if(strncmp(&sv.data[i], str, length) == 0) return i;
+    }
+
+    return STRING_VIEW_NPOS;
+}
+
 
 #endif
--- test/test.c
+++ test/test.c
@@ -2,169 +2,170 @@
 #include <string.h>
 #include <assert.h>
 
+#define STRING_VIEW_IMPLEMENTATION
 #include "../string_view.h"
 
 #define PRINT_INFO_MESSAGE(msg) printf("\e[1;32m%s\e[0m\n", msg);
 
-void test_sv_trim(){
-    string_view sv = sv_from_parts("  Hello World!!!\n\n  ", 20);
+void test_string_view_trim(){
+    string_view_t sv = new_string_view("  Hello World!!!\n\n  ", 20);
     
-    PRINT_INFO_MESSAGE("Running `sv_trim` test...");
+    PRINT_INFO_MESSAGE("Running `string_view_trim` test...");
     
-    sv_trim(&sv);
-    assert(strncmp(sv_data(sv), "Hello World!!!", sv_size(sv)) == 0);
+    string_view_trim(&sv);
+    assert(strncmp(string_view_data(sv), "Hello World!!!", string_view_size(sv)) == 0);
 }
 
-void test_sv_remove_prefix(){
-    string_view sv = sv_from_str("http://example.com");
+void test_string_view_remove_prefix(){
+    string_view_t sv = new_string_view_from_cstr("http://example.com");
     
-    PRINT_INFO_MESSAGE("Running `sv_remove_prefix` tests...");
+    PRINT_INFO_MESSAGE("Running `string_view_remove_prefix` tests...");
 
-    sv_remove_prefix(&sv, 7);
-    assert(strncmp(sv_data(sv), "example.com" , sv_size(sv)) == 0);
+    string_view_remove_prefix(&sv, 7);
+    assert(strncmp(string_view_data(sv), "example.com" , string_view_size(sv)) == 0);
 
-    sv_remove_prefix(&sv, sv_size(sv)+1);
-    assert(sv_is_null(sv) == true);
+    string_view_remove_prefix(&sv, string_view_size(sv)+1);
+    assert(string_view_is_empty(sv) == true);
 }
 
-void test_sv_remove_suffix(){
-    string_view sv = sv_from_str("test-name-file.txt");
+void test_string_view_remove_suffix(){
+    string_view_t sv = new_string_view_from_cstr("test-name-file.txt");
 
-    PRINT_INFO_MESSAGE("Running `sv_remove_suffix` tests...");
+    PRINT_INFO_MESSAGE("Running `string_view_remove_suffix` tests...");
 
-    sv_remove_suffix(&sv, 4);
-    assert(strncmp(sv_data(sv), "test-name-file", sv_size(sv)) == 0);
+    string_view_remove_suffix(&sv, 4);
+    assert(strncmp(string_view_data(sv), "test-name-file", string_view_size(sv)) == 0);
 
-    sv_remove_prefix(&sv, sv_size(sv)+1);
-    assert(sv_is_null(sv) == true);
+    string_view_remove_prefix(&sv, string_view_size(sv)+1);
+    assert(string_view_is_empty(sv) == true);
 }
 
-void test_sv_swap(){
-    string_view sv1 = sv_from_str("Hello");
-    string_view sv2 = sv_from_str("Hello1");
+void test_string_view_swap(){
+    string_view_t sv1 = new_string_view_from_cstr("Hello");
+    string_view_t sv2 = new_string_view_from_cstr("Hello1");
 
-    PRINT_INFO_MESSAGE("Running `sv_swap` test...");
+    PRINT_INFO_MESSAGE("Running `string_view_swap` test...");
 
-    sv_swap(&sv1, &sv2);
+    string_view_swap(&sv1, &sv2);
 
     assert(
-        strncmp(sv_data(sv1), "Hello1", sv_size(sv1)) == 0 &&
-        strncmp(sv_data(sv2), "Hello", sv_size(sv2)) == 0
+        strncmp(string_view_data(sv1), "Hello1", string_view_size(sv1)) == 0 &&
+        strncmp(string_view_data(sv2), "Hello", string_view_size(sv2)) == 0
     );
 }
 
-void test_sv_copy(){
+void test_string_view_copy(){
 
     char buffer[30];
-    string_view sv = sv_from_str("Hello World");
+    string_view_t sv = new_string_view_from_cstr("Hello World");
 
-    PRINT_INFO_MESSAGE("Running `sv_copy` test...");
+    PRINT_INFO_MESSAGE("Running `string_view_copy` test...");
 
-    sv_copy(sv, buffer, 5, 6);
+    string_view_copy(sv, buffer, 5, 6);
     assert(strncmp(buffer, "World", 5) == 0);
 
-    sv_copy(sv, buffer, 12, 6);
+    string_view_copy(sv, buffer, 12, 6);
     assert(strncmp(buffer, "World", 5) == 0);
 }
 
-void test_sv_substr(){
+void test_string_view_substr(){
     
-    string_view sv = sv_from_str("http://example.com/");
+    string_view_t sv = new_string_view_from_cstr("http://example.com/");
     
-    PRINT_INFO_MESSAGE("Running `sv_substr` tests...");
+    PRINT_INFO_MESSAGE("Running `string_view_substr` tests...");
     
-    string_view result = sv_substr(sv, 7, 11);
-    assert(strncmp(sv_data(result), "example.com", sv_size(result)) == 0);
+    string_view_t result = string_view_substr(sv, 7, 11);
+    assert(strncmp(string_view_data(result), "example.com", string_view_size(result)) == 0);
 
-    result = sv_substr(sv, 7, 15);
-    assert(strncmp(sv_data(result), "example.com/", sv_size(result)) == 0);
+    result = string_view_substr(sv, 7, 15);
+    assert(strncmp(string_view_data(result), "example.com/", string_view_size(result)) == 0);
 
-    result = sv_substr(sv, sv_size(sv)+1, 11);
-    assert(sv_is_null(result) == true);
+    result = string_view_substr(sv, string_view_size(sv)+1, 11);
+    assert(string_view_is_empty(result) == true);
 }
 
-void test_sv_compare(){
+void test_string_view_compare(){
 
-    string_view sv1 = sv_from_parts("Hello", 5);
-    string_view sv2 = sv_from_parts("Hello World", 11);
+    string_view_t sv1 = new_string_view("Hello", 5);
+    string_view_t sv2 = new_string_view("Hello World", 11);
 
-    PRINT_INFO_MESSAGE("Running `sv_compare` tests...");
+    PRINT_INFO_MESSAGE("Running `string_view_compare` tests...");
 
-    assert(sv_compare(sv1, sv2) < 0);
-    assert(sv_compare(sv_substr(sv2, 0, sv_size(sv1)), sv1) == 0);
-    assert(sv_compare(sv1, sv_substr(sv2, 0, 3)) > 0);
+    assert(string_view_compare(sv1, sv2) < 0);
+    assert(string_view_compare(string_view_substr(sv2, 0, string_view_size(sv1)), sv1) == 0);
+    assert(string_view_compare(sv1, string_view_substr(sv2, 0, 3)) > 0);
 }
 
-void test_sv_equal(){
-    string_view sv1 = sv_from_parts("Hello", 5);
-    string_view sv2 = sv_from_parts("Hello World", 11);
+void test_string_view_equal(){
+    string_view_t sv1 = new_string_view("Hello", 5);
+    string_view_t sv2 = new_string_view("Hello World", 11);
 
-    PRINT_INFO_MESSAGE("Running `sv_equal` tests...");
+    PRINT_INFO_MESSAGE("Running `string_view_equal` tests...");
 
-    assert(sv_equal(sv1, sv2) == false);
-    assert(sv_equal(sv1, sv_substr(sv2, 0, 5)) == true);
+    assert(string_view_equal(sv1, sv2) == false);
+    assert(string_view_equal(sv1, string_view_substr(sv2, 0, 5)) == true);
 }
 
-void test_sv_starts_with(){
+void test_string_view_starts_with(){
 
-    string_view sv = sv_from_str("http://google.com");
+    string_view_t sv = new_string_view_from_cstr("http://google.com");
 
-    PRINT_INFO_MESSAGE("Running `sv_starts_with` tests..."); 
+    PRINT_INFO_MESSAGE("Running `string_view_starts_with` tests..."); 
 
-    assert(sv_starts_with(sv, "http://", 7) == true);
-    assert(sv_starts_with(sv, "http://google.com/", sv_size(sv)+1) == false);
-    assert(sv_starts_with(sv, "", 5) == false);
+    assert(string_view_starts_with(sv, "http://", 7) == true);
+    assert(string_view_starts_with(sv, "http://google.com/", string_view_size(sv)+1) == false);
+    assert(string_view_starts_with(sv, "", 5) == false);
 }
 
-void test_sv_ends_with(){
+void test_string_view_ends_with(){
 
-    string_view sv = sv_from_str("http://google.com");
+    string_view_t sv = new_string_view_from_cstr("http://google.com");
 
-    PRINT_INFO_MESSAGE("Running `sv_ends_with` tests..."); 
+    PRINT_INFO_MESSAGE("Running `string_view_ends_with` tests..."); 
 
-    assert(sv_ends_with(sv, ".com", 4) == true);
-    assert(sv_ends_with(sv, "http://google.com/", sv_size(sv)+1) == false);
-    assert(sv_ends_with(sv, "", 5) == false);
+    assert(string_view_ends_with(sv, ".com", 4) == true);
+    assert(string_view_ends_with(sv, "http://google.com/", string_view_size(sv)+1) == false);
+    assert(string_view_ends_with(sv, "", 5) == false);
 }
 
-void test_sv_find_char(){
-    string_view sv = sv_from_str("This is a string");
+void test_string_view_find_char(){
+    string_view_t sv = new_string_view_from_cstr("This is a string");
 
-    PRINT_INFO_MESSAGE("Running `sv_find_char` tests...");
+    PRINT_INFO_MESSAGE("Running `string_view_find_char` tests...");
 
-    assert(sv_find_char(sv, 'a', 10) == -1);
-    assert(sv_find_char(sv, '.', 0) == -1);
-    assert(sv_find_char(sv, 'a', -1) == -1);
-    assert(sv_find_char(sv, 'a', 7) == 8);
+    assert(string_view_find_char(sv, 'a', 10) == STRING_VIEW_NPOS);
+    assert(string_view_find_char(sv, '.', 0) == STRING_VIEW_NPOS);
+    assert(string_view_find_char(sv, 'a', 100) == STRING_VIEW_NPOS);
+    assert(string_view_find_char(sv, 'a', 7) == 8);
 
 }
 
-void test_sv_find_str(){
-    string_view sv = sv_from_str("This is a string");
+void test_string_view_find_str(){
+    string_view_t sv = new_string_view_from_cstr("This is a string");
 
-    PRINT_INFO_MESSAGE("Running `sv_find_str` tests...");
+    PRINT_INFO_MESSAGE("Running `string_view_find_str` tests...");
 
-    assert(sv_find_str(sv, "string", 10) == 10);
-    assert(sv_find_str(sv, "pippo", 0) == -1);
-    assert(sv_find_str(sv, "Hello", -1) == -1);
-    assert(sv_find_str(sv, "is", 4) == 5);
+    assert(string_view_find_str(sv, "string", 10) == 10);
+    assert(string_view_find_str(sv, "pippo", 0) == STRING_VIEW_NPOS);
+    assert(string_view_find_str(sv, "Hello", 100) == STRING_VIEW_NPOS);
+    assert(string_view_find_str(sv, "is", 4) == 5);
 
 }
 
 int main(void){
 
-    test_sv_trim();
-    test_sv_remove_prefix();
-    test_sv_remove_suffix();
-    test_sv_swap();
-    test_sv_copy();
-    test_sv_substr();
-    test_sv_compare();
-    test_sv_equal();
-    test_sv_starts_with();
-    test_sv_ends_with();
-    test_sv_find_char();
-    test_sv_find_str();
+    test_string_view_trim();
+    test_string_view_remove_prefix();
+    test_string_view_remove_suffix();
+    test_string_view_swap();
+    test_string_view_copy();
+    test_string_view_substr();
+    test_string_view_compare();
+    test_string_view_equal();
+    test_string_view_starts_with();
+    test_string_view_ends_with();
+    test_string_view_find_char();
+    test_string_view_find_str();
 
     PRINT_INFO_MESSAGE("\nAll Test passed!!! :)");
 
